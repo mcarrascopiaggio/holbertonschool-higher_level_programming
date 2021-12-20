c511c53b68831c9a89b6e22a68093f078a5994be
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-12-20 09:08:19 -0800

    task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Pythion is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = "object-oriented programming with Python"
+str = str[40:67] + str[107:112] +str[107] +str[:7]
 print(str)
