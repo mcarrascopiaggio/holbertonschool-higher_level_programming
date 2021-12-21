1d755fc7cc0e832f5e8161640ed01d4ed4d6247d
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-12-21 09:52:37 -0800

    task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        lastd = number % -10
-    else:
-        lastd = number % 10
+        number = number * -1
+    lastd = number % 10
     print("{:d}".format(lastd), end='')
     return (lastd)
