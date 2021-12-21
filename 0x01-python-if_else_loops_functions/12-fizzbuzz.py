e980b6c5b7f92d657f5f50f8ce02e12b8fbd9be5
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-12-21 10:20:03 -0800

    task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in (1, 101):
-        if i % 5 == 0 and i % 3 == 0:
+        if i % 15 == 0:
             print("FizzBuzz", end=' ')
         elif i % 5 == 0:
             print("Buzz", end=' ')
