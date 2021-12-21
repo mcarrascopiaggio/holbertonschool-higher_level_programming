b2af4f5ea88f8b818df84df09a476389ffaec2fe
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-12-21 07:40:52 -0800

    format fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def printLowerAlpha():
-    for i in range(97,123):
-        print(chr(i), end = '')
-printLowerAlpha()
+for i in range(97, 123):
+    print("{:c}".format(i), end='')
