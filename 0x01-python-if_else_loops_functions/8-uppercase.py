4648c4d74390269b9509bbad707817f9739f98c0
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-12-21 09:23:15 -0800

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        i = ord(i)
+        if i in range(97, 123):
+            i = i - 32
+        print("{:c}".format(i), end='')
+    print(" ")
+
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
