0db2e8c596bb92feda65fcfdec96a5da0c7abdc2
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-12-21 13:18:33 -0800

    add n extreme values

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     large = len(str)
-    str = str[0:n] + str[n+1:]
+    if n > 0 and n <= large:
+        str = str[0:n] + str[n+1:]
     return(str)
