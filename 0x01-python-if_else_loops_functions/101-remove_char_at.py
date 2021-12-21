5d80297a1f5cec30cf1087b7ea9ccc506b07608f
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-12-21 13:27:20 -0800

    task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,4 +3,6 @@ def remove_char_at(str, n):
     large = len(str)
     if n > 0 and n <= large:
         str = str[0:n] + str[n+1:]
-    return(str)
+        return (str)
+    else:
+        return(str)
