f063aa584dfad7895d584499365d4a09fbeb21a8
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-12-23 08:42:50 -0800

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    le = len(argv) -1
+    le = len(argv) - 1
     if le == 0:
         print("0 arguments.")
     elif le == 1:
