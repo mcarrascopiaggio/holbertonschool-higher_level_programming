2743fbb5436de5935fa80751331a7d2a9f55cbd9
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-01-03 08:04:56 -0800

    add exist condition task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    lg = len(my_list)
-    for i in range(lg):
-        print("{:d}".format(my_list[-i-1]))
+    if my_list:
+        lg = len(my_list)
+        for i in range(lg):
+            print("{:d}".format(my_list[-i-1]))
