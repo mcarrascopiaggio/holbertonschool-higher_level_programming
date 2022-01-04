4c3caf0b6a27395fceb3624d632dddffaf6e9882
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-01-03 17:48:51 -0800

    task 6 - still in process

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in range(len(matrix)):
-        for j in range(len(matrix[row]):
-                print(" ". end="")
-                print("{:d}".format([i][j])::x)
+    if not matrix:
+        print()
+    for row in matrix:
+        col = 0
+        for i in row:
+            if col != 0:
+                print(' ', end="")
+            print("{:d}".format(i), end='')
+            col = col + 1
+        print()
