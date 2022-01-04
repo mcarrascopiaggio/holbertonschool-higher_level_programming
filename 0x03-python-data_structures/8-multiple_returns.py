c8ef9aebb3ec9998d0655e28b1ff0fd57c17c2f1
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-01-03 16:54:59 -0800

    fix case 0

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        sentence[0] = None
-    my_tuple = (len(sentence), sentence[0])
-    return my_tuple
+        return 0, None
+    else:
+        my_tuple = (len(sentence), sentence[0])
+        return my_tuple
