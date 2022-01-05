92ef431679311ed3de0acb95a13217bf76fe1044
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-01-04 17:41:58 -0800

    task 101 fix prototype

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
+def square_matrix_map(matrix=[]):    
     new_matrix = []
     for i in matrix:
         new_matrix.append(list(map(lambda x: x*x, i)))
