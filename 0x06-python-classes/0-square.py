471d7160b74c5a31e250d43f2d004d324e5efb54
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-01-18 08:42:00 -0800

    task 2

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/ptyhon3
+""" Write an empty class Square that defines a square """
+
+
+class Square:
+    """ This class contain all squares """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Write a class Square that defines a square"""
+
+
+class Square:
+    """Create a class empty """
+    def __init__(self, size):
+        """Have a private attribute size"""
+        self.__size = size
