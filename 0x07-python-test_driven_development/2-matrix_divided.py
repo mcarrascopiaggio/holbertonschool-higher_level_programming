713ea239e14d2d075112440b2b06c06f7e98af28
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-01-22 10:10:16 -0800

    first version 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+this module has a fuction tha divide a matrix in a div
+return a new matrix with two decimal
+Raise an error matrix has a type different from int and float
+Raise an error if the len of list in list are different
+Raise an error if div is 0
+"""
+
+
 def matrix_divided(matrix, div):
     """ This function divide a matrix by a number
     and retur a new matrix with the result.
@@ -9,25 +18,16 @@ def matrix_divided(matrix, div):
 
     if div == 0:
         raise("division by zero")
+    if type(div) is not int and type(div) is not float:
+        raise(" ")
     for i in matrix:
-        if len(matrix[i]) != len_matrix:
+        if len(i) != len_matrix:
             raise("Each row of the matrix must have the same size")
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if type(matrix[i][j]) != int and type(matrix[i][j]) != float:
+                raise("matrix must be a matrix (list of lists) of\
+                        integers/floats")
     for i in matrix:
-        for j in i:
-            if type(matrix[i][j]) is not int and
-            type(matrix[i][j]) is not float:
-                raise("matrix must be a matrix (list of lists) of integers/floats")
-    for i in matrix:
-        new_matrix.append(list(map(lambda x: x/div, i)))
+        new_matrix.append(list(map(lambda x: round(x/div, 2), i)))
     return new_matrix
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
-new_matrix = (matrix_divided(matrix, 3))
-roun_new_matrix = [round (num, 2) for num in new_matrix]
-print(roun_new_matrix)
-print("the leng of the matrix is {:d}".format(len(matrix)))
-print("the leng of the matrix list 1 is {:d}".format(len(matrix[0])))
-print("the leng of the new matrix is {:d}".format(len(new_matrix)))
