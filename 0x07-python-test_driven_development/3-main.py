41cfb7d947e594c4d78903d89227717376a68d1a
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-01-22 19:14:46 -0800

    task 3 test passed

--- 0x07-python-test_driven_development/3-main.py
+++ 0x07-python-test_driven_development/3-main.py
@@ -8,3 +8,13 @@ try:
     say_my_name(12, "White")
 except Exception as e:
     print(e)
+try:
+    say_my_name("Marcela", 2022)
+except Exception as e:
+    print(e)
+try:
+    say_my_name()
+except Exception as e:
+    print(e)
+
+
