a7e965b39d4a48440e341516f342c3bb955706a0
Marcela Carrasco Piaggio <91094893+mcarrascopiaggio@users.noreply.github.com>
2022-04-19 19:43:13 +0200

    Delete 0x01-python-if_else_loops_functions directory
    Delete directorio por cambio a 17

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for i in str:
-        i = ord(i)
-        if i in range(97, 123):
-            i = i - 32
-        print("{:c}".format(i), end='')
-    print("")
