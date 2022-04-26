6d8fdf823d36e85f2fe87628ab469411e99a4a37
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-04-26 07:35:17 -0700

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letter in str:
+        letter = ord(letter)
+        if letter in range(97, 123):
+            letter = letter - 32
+        print(f"{letter:c}", end="")
+    print()
