2add4faae789269e3c844f2a7aafd6f437eee319
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-02 09:29:36 -0700

    task without finisih

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info -function that prints some basic info about Py lists
+ * @p: pointer to a objetc
+ * Return: 0
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	void(p);/**ex withoutfinished*/
+}
