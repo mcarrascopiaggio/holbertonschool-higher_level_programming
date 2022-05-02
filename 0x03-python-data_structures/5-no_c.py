6bad89ee35f180d26a55cb3d4360f18a7eb368df
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-02 08:51:27 -0700

    change identation

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for letter in my_string:
-        if letter != "c" or letter != "C":
+        if letter != "c" and letter != "C":
             new_string = new_string + letter
-        return new_string
+    return new_string
