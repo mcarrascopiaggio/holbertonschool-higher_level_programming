a2b392c483c2fb797a0fcba4cbf4663f29c4edf0
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-02 09:14:22 -0700

    add main if condition

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    maxI = my_list[0]
-    for i in range(len(my_list)):
-        if my_list[i] > maxI:
-            maxI = my_list[i]
-    return maxI
+    if my_list:
+        maxI = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > maxI:
+                maxI = my_list[i]
+        return maxI
