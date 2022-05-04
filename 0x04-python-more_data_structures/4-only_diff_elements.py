814867bf0aa74ec62a6b9007855b479e37f27a4c
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-04 09:50:06 -0700

    change code

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    l1 = list(set_1)
-    l2 = list(set_2)
-    l12 = l1 + l2
-    uniquel12 = set(l12)
-    return uniquel12
+    return set_1 ^ set_2
