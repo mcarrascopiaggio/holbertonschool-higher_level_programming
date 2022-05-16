7fc01207bfe9ca905728d7ce7af4ea483635fe1b
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-16 09:24:23 -0700

    change new line out of try

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(f"{my_list[i]}", end="")
             nb_print = nb_print + 1
-        print()
     except Exception:
         pass
+    print()
     return(nb_print)
