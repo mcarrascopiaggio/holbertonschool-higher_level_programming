7f4961eab4da77859f9e9c83bf94601af9f56b4d
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-17 09:43:03 -0700

    second

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,7 +2,7 @@
 """Write a class Square that defines a square by: (based on 0-square.py)"""
 
 
-class Square(size):
+class Square:
     """empty class Square with a private attribute"""
     def __init__(self, size):
         """function init Square class with a private attribute size"""
