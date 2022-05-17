a4215fcc6a3675c3feedcfdc478c4901e10aeecb
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-17 09:47:35 -0700

    try with instances form instead of type

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
     def __init__(self, size=0):
         """function init Square class with a private attribute size"""
         self.__size = size
-        if type(self.__size) is not int:
+        if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
         if self.__size <0:
             raise ValueError("size must be >= 0")
