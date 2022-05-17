47b18a7a160d635516b32aac3155632890d9b70e
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-17 10:36:41 -0700

    first version

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Class project - Square"""
+
+
+class Square:
+    """empty class Square with a private attribute"""
+    def __init__(self, size=0):
+        """function init Square class with a private attribute size"""
+        self.__size = size
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        """ retrieve itself"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ property setter """
+        self.__size = size
+
+        if type(self.__size) is not int:
+            raise TypeError('size must be an integer')
+        if self.__size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        """function that eturns the current square area"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for o in range(self.__size):
+                    print("#", end="")
+                print()
