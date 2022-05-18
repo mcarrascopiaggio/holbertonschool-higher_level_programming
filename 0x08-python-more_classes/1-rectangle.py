c31771f4aa1fbad238924b488aa8892cb0f80fc4
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-18 15:03:48 -0700

    change property

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle:
     @property
     def width(self):
         """retrieve it"""
-        return self.__width = width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -26,7 +26,7 @@ class Rectangle:
     @property
     def height(self):
         """ retrieve it"""
-        return self.__height = height
+        return self.__height
 
     @height.setter
     def height(self, value):
