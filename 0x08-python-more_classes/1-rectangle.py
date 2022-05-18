1c7de6af36c491f00b9f37cb88c0d5ffe026c9b3
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-18 15:13:29 -0700

    change order

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
-"""empty class Rectangle that defines a rectangle"""
+"""
+    Function that defines a rectangle
+"""
 
 
 class Rectangle:
-    """Define class Rectangle"""
+    """
+    Class Rectangle
+    """
+
     def __init__(self, width=0, height=0):
-        """Instantiation with optional width and height"""
-        self.__height = height
-        self.__width = width
+        """
+            Definition of class Rectangle.
+                Args:
+                    width: of the rectangle
+                    height: of the rectangle
+        """
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
-        """retrieve it"""
+        """Property to retrive width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """condition"""
-        if not isinstance(value, int):
+        """Property to se width"""
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -25,13 +35,13 @@ class Rectangle:
 
     @property
     def height(self):
-        """ retrieve it"""
+        """Property to retrive height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """conditions"""
-        if not isinstance(value, int):
+        """Property to se height"""
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
