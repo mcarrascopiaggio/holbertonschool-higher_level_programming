b46d471f086598cae3e9c56a71718c70ad3ef918
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-18 15:18:55 -0700

    add cero condition in perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -43,4 +43,6 @@ class Rectangle:
 
     def perimeter(self):
         """that returns the rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (self.__width + self.__height) * 2
