77bba47ec70de385fa279fc1264206bb5c803c2a
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-18 17:01:17 -0700

    change order in init

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Instantiation with optional width and height"""
-        self.__height = height
         self.__width = width
+        self.__height = height
         Rectangle.number_of_instances += 1
 
     @property
