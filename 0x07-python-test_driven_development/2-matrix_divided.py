7cb370ba95c9a2a43a3ebfd0333b312e1637321c
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-20 19:21:30 -0700

    change function

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,33 +10,27 @@ def matrix_divided(matrix, div):
     """
     function that divides all elements of a matrix.
     """
-    new_matrix = []
-    msg_div = "matrix_divided() missing 1 required positional argument: 'div'"
-    msg_m1 = "matrix must be a matrix (list of lists) of integers/floats"
-    msg_m2 = "Each row of the matrix must have the same size"
+    Error = "matrix must be a matrix (list of lists) of integers/floats"
+    Msg = "Each row of the matrix must have the same size"
+
+    if not isinstance(matrix, list) or len(matrix) == 0 or not matrix[0]:
+        raise TypeError(Error)
+    for row in matrix:
+        if len(row) == 0:
+            raise TypeError(Error)
+        for x in row:
+            if type(x) is not int and type(x) is not float:
+                raise TypeError(Error)
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
+    len_row = len(matrix[0])
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if not div:
-        raist
-        TypeError(msg_div)
-
-    if type(matrix) is list:
-        lenght_row = len(matrix[0])
-        for row in matrix:
-            """check if all rows are same lenght"""
-            if len(row) != lenght_row:
-                raise TypeError(msg_m2)
-            """check if each row is a list"""
-            if type(row) is not list:
-                raise TypeError(msg_m1)
-            """check if each element is an integer or float"""
-            for element in row:
-                if type(element) is not int and type(element) is not float:
-                    raise TypeError(msg_m1)
-            """divide each element, build new row and adds it to new matrix"""
-            new_matrix.append(list(map(lambda x: round(x/div, 2), row)))
-    else:
-        raise TypeError(msg_m1)
+    lenght_row = len(matrix[0])
+    for row in matrix:
+        if len(row) != lenght_row:
+            raise TypeError(Msg)
+    new_matrix = []
+    for row in matrix:
+        new_matrix.append(list(map(lambda x: round(x/div, 2), row)))
     return new_matrix
