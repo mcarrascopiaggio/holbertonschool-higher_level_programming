fd30082938ec9aac7a783dac1acc0ddf1c19543a
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-30 01:22:02 -0700

    first version

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+0-lookup.py
+inheritance
+class
+"""
+
+
+def lookup(obj):
+    """ returns the list of
+    available attattributes and methods of an object:
+    """
+    return dir(obj)
