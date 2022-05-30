8f2ae218216eabb8762f16173c0f284268c8a4f5
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-30 03:25:14 -0700

    first version

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Write an empty class BaseGeometry
+third line
+four line
+"""
+
+
+class BaseGeometry():
+    """
+    empty class
+    """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Write an empty class BaseGeometry
+third line
+four line
+"""
+
+
+class BaseGeometry():
+    """
+    empty class
+    """
+
+    def area(self):
+        """raises an Exception with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
