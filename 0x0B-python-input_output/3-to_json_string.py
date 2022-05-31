c46d1d4120ccb3b068e997eea0570d2da98a1aba
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-31 06:24:33 -0700

    first version output & pycodestyle ok

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+To JSON string
+Write a function that returns
+the JSON representation of an object (string)
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string)
+    Arg my_obj
+    """
+    return json.dumps(my_obj)
