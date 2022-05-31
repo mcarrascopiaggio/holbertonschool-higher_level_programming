c05db6fd2eaeca64da6cb306b39ac4906991227f
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-31 15:08:18 -0700

    first version

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Write a class Student that defines a student by
+public instances & public method
+"""
+
+
+class Student():
+    """
+    class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation with first_name, last_name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        function that writes an Object to a text file, using a JSON rep:
+        """
+        aux = {}
+        if type(attrs) == list:
+            for key in attrs:
+                if type(key) != str:
+                    return self.__dict__
+                if key in self.__dict__:
+                    aux[key] = self.__dict__[key]
+            return aux
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        replaces all attributes of the Student instance
+        """
+        for i in json:
+            self.__dict__[i] = json[i]
