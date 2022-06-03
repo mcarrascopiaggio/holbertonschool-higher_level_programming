1516aef41a32b2cee7d13033042c8c89a5ae7d4c
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-06-03 07:18:15 -0700

    add test files with header

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Tests for Base"""
+
+
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class Test_Base(unittest.TestCase):
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Tests for Square"""
+
+
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class Test_Square(unittest.TestCase):
