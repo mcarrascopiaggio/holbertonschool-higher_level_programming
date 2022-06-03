76166a503f7dfed3593b89713ee6148a1a56454a
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-06-03 06:29:42 -0700

    jason tranformation output in different order

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@ Write the first class Base
 """
 
 
+import json
+
+
 class Base():
     """
     Create a file named
@@ -20,3 +23,13 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
