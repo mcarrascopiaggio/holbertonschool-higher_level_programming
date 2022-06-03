96e10076963b1347d957ad48a98e78441f691491
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-06-03 06:51:35 -0700

    add json trans

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,18 @@ class Base():
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        writes the JSON string representation of list_objs to a file:
+        """
+        filename = cls.__name__ + ".json"
+        my_List = []
+        with open(filename, mode='a', encoding="utf-8") as myFile:
+            if list_objs is None:
+                return myFile.write("")
+            else:
+                for items in list_objs:
+                    my_List.append(items.to_dictionary())
+                return myFile.write(cls.to_json_string(my_List))
