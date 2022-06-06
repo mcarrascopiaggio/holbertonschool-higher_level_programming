d778f7c103d0579258d33a6c94d23ce16b001e2b
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-06-06 13:46:35 -0700

    add test

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -20,7 +20,7 @@ class Test_Square(unittest.TestCase):
         self.assertRaises(TypeError, Square, ())
 
     def test_error(self):
-        """ 
+        """
         test error in size
         """
         self.assertRaises(ValueError, Square, 0)
@@ -32,7 +32,7 @@ class Test_Square(unittest.TestCase):
         self.assertRaises(TypeError, Square, 1.0)
 
     def test_size(self):
-        """ 
+        """
         test error in size
         """
         Base._Base__nb_objects = 0
@@ -45,3 +45,22 @@ class Test_Square(unittest.TestCase):
         """
         s1 = Square(2, 0, 0, 45)
         self.assertEqual(s1.__str__(), "[Square] (45) 0/0 - 2")
+
+    def test_update(self):
+        """
+        check update
+        """
+        s1 = Square(5)
+        s1.update(1, 2)
+        self.assertEqual(s1.__str__(), "[Square] (10) 0/0 - 5")
+
+    def test_to_dictionary(self):
+        """
+        check to dictionary
+        """
+        s1 = Square(10, 2, 1)
+        s1_dictionary = s1.to_dictionary()
+        s2 = Square(1, 1)
+        s2.update(**s1_dictionary)
+        self.assertFalse(s1 is s2)
+        self.assertDictEqual(s1_dictionary, s2.to_dictionary())
