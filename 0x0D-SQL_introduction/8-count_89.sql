4d767ec9e72047a83de49a688c3329e41b04c2c5
root <mcarrascopiaggio@gmail.com>
2022-07-05 11:17:34 -0700

    first version

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- task 8. Count 89
+-- Write a script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in
+SELECT COUNT(id = 89)
+FROM first_table;
