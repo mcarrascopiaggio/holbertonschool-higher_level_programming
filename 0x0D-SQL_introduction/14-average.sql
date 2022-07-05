a29d2570aeefcebd029b914ac39c4839df98bed3
root <mcarrascopiaggio@gmail.com>
2022-07-05 12:00:21 -0700

    add first version

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- task 14. Count 89
+-- Write a script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in
+SELECT AVG(score) AS average FROM second_table;
