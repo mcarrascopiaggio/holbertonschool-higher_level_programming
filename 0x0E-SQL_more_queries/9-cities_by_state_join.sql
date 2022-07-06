74462d0b55eccb5afed080d01b2433b331c53797
root <mcarrascopiaggio@gmail.com>
2022-07-06 12:38:04 -0700

    add a ,

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Cities of California
 -- Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT cities.id, cities.name cities.states 
+SELECT cities.id, cities.name, cities.states 
 FROM cities
 INNER JOIN states
 ON states.id=cities.state_id
