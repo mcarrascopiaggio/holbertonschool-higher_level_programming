9a2a5f0df40aad4d6d70ed6fc13f1ff6373ba487
root <mcarrascopiaggio@gmail.com>
2022-07-06 11:54:39 -0700

    add a space in PRIMARY KEY

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- Write a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-	id INT PRIMARYKEY AUTO_INCREMENT,
+	id INT PRIMARY KEY AUTO_INCREMENT,
 	state_id FOREIGN KEY(state_id) REFERENCES states(id),
 	name VARCHAR(256) NOT NULL	
 );
