2c5a9afc33a3e84dd031e2f16a55332bb2eec783
root <mcarrascopiaggio@gmail.com>
2022-07-06 17:11:35 -0700

    first try

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- Genre ID by show
+-- Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa
+SELECT tv_shows.title  AS tittle, tv_show_genres.genre_id AS genre_id
+FROM tv_show_genres
+INNER JOIN tv_shows
+ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id
