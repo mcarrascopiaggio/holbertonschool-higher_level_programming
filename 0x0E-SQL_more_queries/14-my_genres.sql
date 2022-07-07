1f43f338e326fca01e8125fc248d601190a67a9a
root <mcarrascopiaggio@gmail.com>
2022-07-06 18:15:29 -0700

    first

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- My genres
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 13-count_shows_by_genre.sql)
+SELECT name
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_genres.id=tv_show_genres.genre_id
+JOIN tv_shows
+ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title='Dexter'
+ORDER BY tv_genres.name
