f2e87039e3faf786f5f90dc93320442d06f558fc
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-08-01 11:41:51 -0700

    fix format things

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,10 +3,10 @@ const { argv } = require('process');
 const side = parseInt(argv[2]);
 let i;
 
-if(isNaN(side)){
+if (isNaN(side)) {
   console.log('Missing size');
 } else {
-  for ( i = 0; i < side; i++) {
+  for (i = 0; i < side; i++) {
     console.log('X'.repeat(side));
   }
 }
