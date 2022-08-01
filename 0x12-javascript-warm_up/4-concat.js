182ce39193a30543a8e95c22cb3d30df83002678
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-08-01 10:22:35 -0700

    seccond version

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-let myvar = 'is';
-if (process.argv.length === 4) {
-  console.log(process.argv[2] + ' ' + myvar + ' ' + process.argv[3]);
-} else if (process.argv === 3){
-  console.log(process.argv[2] + ' ' + myvar + ' ' + 'undefined');
-} else {
-  console.log(process.argv[2]);
+const und = 'undefined';
+if (!process.argv[2]) {
+  process.argv[2] = und;
 }
+if (!process.argv[3]) {
+  process.argv[3] = und;
+}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
