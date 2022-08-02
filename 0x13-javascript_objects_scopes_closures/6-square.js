ac721ff5e64870945b5cd73367711ef080548074
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-08-02 13:17:09 -0700

    first version

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const Square1 = require('./5-square');
+module.exports = class Square extends Square1 {
+  charPrint (c) {
+    let i;
+    const text = 'X';
+    if (c) {
+      for (i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.height));
+      }
+    } else {
+      for (i = 0; i < this.width; i++) {
+        console.log(text.repeat(this.height));
+      }
+    }
+  }
+};
