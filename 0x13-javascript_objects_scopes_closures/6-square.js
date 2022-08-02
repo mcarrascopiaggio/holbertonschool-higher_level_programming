cc1f7910beeb0995c744d4067882693655110f81
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-08-02 13:30:45 -0700

    fix format issues

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,6 +4,7 @@ class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
+
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.height));
