a8635c85e807ab59f4ff2b701376028804206471
root <mcarrascopiaggio@gmail.com>
2022-08-04 09:16:41 -0700

    first version

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Write a script that lists all states from the database hbtn_0e_0_usa:
+"""
+
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+state = str(argv[4])
+# crear un objeto de cursor.
+cur = db.cursor()
+# objeto de cursor y llamar a la función 'ejecutar'.
+# La función de ejecución requiere un parámetro, la consulta.
+query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+cur.execute(query, (state,))
+# Después de ejecutar cualquier declaración SELECT se necesita mostrar los res
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+# Close all cursors
+cur.close()
+# Close all databases
+db.close()
