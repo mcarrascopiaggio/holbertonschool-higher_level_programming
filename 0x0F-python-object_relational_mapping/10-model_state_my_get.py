fbebeac17865a095887947e8378d12328d3a7caf
root <mcarrascopiaggio@gmail.com>
2022-08-05 12:30:00 -0700

    first try

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Contains `a`
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+           'mysql+mysqldb://{}:{}@localhost/{}'.
+           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    if sys.argv[4]:
+        search = sys.argv[4]
+        state = session.query(State).filter(State.name == search).first()
+
+        if (state):
+            print(state.id)
+        else:
+            print("Not found")
+    session.close()
