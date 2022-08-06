2945f906fb77ade55e81d757625b90e99bfea60c
root <mcarrascopiaggio@gmail.com>
2022-08-06 11:11:53 -0700

    change format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,20 +12,20 @@ if __name__ == "__main__":
         user=argv[1],
         passwd=argv[2],
         db=argv[3])
-state = str(argv[4])
-# crear un objeto de cursor.
-cur = db.cursor()
-# objeto de cursor y llamar a la función 'ejecutar'.
-# La función de ejecución requiere un parámetro, la consulta.
-query = """SELECT * FROM states
-        WHERE name LIKE BINARY '{}'
-        ORDER BY id ASC""".format(state)
-cur.execute(query)
-# Después de ejecutar cualquier declaración SELECT se necesita mostrar los res
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-# Close all cursors
-cur.close()
-# Close all databases
-db.close()
+    state = str(argv[4])
+    # crear un objeto de cursor.
+    cur = db.cursor()
+    # objeto de cursor y llamar a la función 'ejecutar'.
+    # La función de ejecución requiere un parámetro, la consulta.
+    query = """SELECT * FROM states
+            WHERE name LIKE BINARY '{}'
+            ORDER BY id ASC""".format(state)
+    cur.execute(query)
+    # Después de ejecutar cualquier declaración SELECT se necesita mostrar los
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()
