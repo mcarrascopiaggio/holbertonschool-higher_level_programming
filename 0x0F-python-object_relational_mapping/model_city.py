0a818f3f524d03a47c601916628b43e2e054fc96
root <mcarrascopiaggio@gmail.com>
2022-08-06 11:30:50 -0700

    add import

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,7 +3,7 @@
 Write a python file that contains the class definition of a State
 instance Base = declarative_base():
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
