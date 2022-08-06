78ee2111aeb337ffe97e1035e4e02e0664d3c34e
root <mcarrascopiaggio@gmail.com>
2022-08-06 11:42:09 -0700

    add BINARY condition to LIKE

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,8 +13,9 @@ if __name__ == "__main__":
         passwd=argv[2],
         db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states
-                WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute(
+        "SELECT * FROM states\
+        WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     if rows:
         for row in rows:
