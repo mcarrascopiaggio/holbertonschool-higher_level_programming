422943460df526ec1b92ae998ad967496da1ec37
root <mcarrascopiaggio@gmail.com>
2022-08-06 11:02:42 -0700

    fix format

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,10 +13,10 @@ if __name__ == "__main__":
         user=argv[1],
         passwd=argv[2],
         db=argv[3])
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-db.close()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
