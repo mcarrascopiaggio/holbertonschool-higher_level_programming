9c4514a68ca793a7d2fba1343aceec6619d1bc5d
root <mcarrascopiaggio@gmail.com>
2022-08-06 11:26:50 -0700

    change algo 4 req issues

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,15 +12,11 @@ if __name__ == "__main__":
            'mysql+mysqldb://{}:{}@localhost/{}'.
            format(sys.argv[1], sys.argv[2], sys.argv[3]),
            pool_pre_ping=True)
-    # Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    flag = False
-    for state in session.query(State).order_by(State.id).all():
+    state = session.query(State).order_by(State.id).first()
+    if state:
         print(f"{state.id}: {state.name}")
-        flag = True
-        if (flag):
-            break
+    else:
+        print("Nothing")
     session.close()
