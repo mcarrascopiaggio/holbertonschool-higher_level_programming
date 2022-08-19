362334a4cf46d82cf3ecb82774f7d4449f6a7f18
root <mcarrascopiaggio@gmail.com>
2022-08-19 09:26:51 -0700

    first version

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 """
-Write a Python script that takes in a URL, sends a request to the URL and
-displays the value of the X-Request-Id variable
-found in the header of the response.
+Write a Python script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter,
+displays the body of the response (decoded in utf-8
+- The email must be sent in the email variable
 - You must use the packages urllib and sys
-- You are not allow to import packages other than urllib and sys
-- The value of this variable is different for each request
+- You are not allowed to import packages other than urllib and sys
 - You don’t need to check arguments passed to the script (number or type)
-- You must use a with statement
+- You must use the with statement
 """
 
 
 import urllib.request
 from sys import argv
+import urllib.parse
 
-if __name__=="__main__":
-    with urllib.request.urlopen(argv[1]) as response:
-	print(responde.headers.get("X-Request-Id"))
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    values = {"email": email}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode('utf-8'))
