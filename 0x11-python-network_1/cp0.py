c39fba9e0afa0bb26d1c001dd379c829ed491df4
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-08-19 16:08:20 +0000

    first version - in progress

--- 0x11-python-network_1/cp0.py
+++ 0x11-python-network_1/cp0.py
@@ -6,11 +6,12 @@ Python script that fetches https://intranet.hbtn.io/status
 """
 
 
+import shutil
+import tempfile
 import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-	print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    	with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        	shutil.copyfileobj(response, tmp_file)
+
+	with open(tmp_file.name) as html:
