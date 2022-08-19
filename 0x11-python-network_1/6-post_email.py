baf328fe9c9797a3d9853e31c6f2d42ae1051902
root <mcarrascopiaggio@gmail.com>
2022-08-19 10:24:51 -0700

    first version

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+takes in a URL and an email address, sends a POST request to the passed URL
+with the email as a parameter, and finally displays the body of the response.
+- The email must be sent in the variable email
+- You must use the packages requests and sys
+- You are not allowed to import packages other than requests and sys
+- You don’t need to error check arguments passed to the script (number or type)
+"""
+
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    mail = argv[2]
+    req = requests.post(url, data={'email': mail})
+    print(req.text)
