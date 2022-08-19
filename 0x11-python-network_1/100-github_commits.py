8b7cfd1cb9aacc0a49abd83f15e2cea1c51e137b
root <mcarrascopiaggio@gmail.com>
2022-08-19 11:13:07 -0700

    first try

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+takes your GitHub credentials (username and password) and uses the
+GitHub API to display your id
+"""
+
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    repository = argv[1]
+    owner = argv[2]
+    ur = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repository)
+    req = requests.get(ur)
+    data = req.json()
+    i = 0
+    for d in data:
+        if i > 10:
+            print("{}: {}".format(x.get("sha"),
+                  x.get('commit').get('author').get('name')))
+            i = i + 1
