2b55bda2a84d0b9945a3ba6d7fc89cc1a3dd5f44
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-09-12 18:09:32 +0000

    first version

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+Write a script that reads and prints the content of a file.
+*/
+
+const argv = process.argv;
+const fs = require('fs');
+fs.readFile(argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
