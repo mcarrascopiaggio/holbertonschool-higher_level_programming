1d759fb71e226f470a258c67bf5e3d3ddf129e64
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-09-12 18:18:35 +0000

    named before env var

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,9 +4,12 @@ Write a script that writes a string to a file.
 Arg[2]= path of the file
 Arg[3]= content of the file
 */
-const argv = process.argv;
+
+const fileName = process.argv[2];
+const content = process.argv[3];
 const fs = require('fs');
-fs.writeFile(argv[2], argv[3], 'utf8', function (err, data) {
+
+fs.writeFile(fileName, content, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   }
